5de4b81e8ae918f3026f09f668bc0a974aea8635
Ahmed Abdelwanes <ahmedabdalwanes842@gmail.com>
2025-04-07 21:12:23 +0200

    ad record

--- js/list.js
+++ js/list.js
@@ -1,26 +1,26 @@
 let allRecord = [
+  {
+    name: "Azan",
+    artist: "Fourth Singer",
+    img: "images/4.jpg",
+    src: "../records/1.mp3"
+  },
+  {
+    name: "quraan1",
+    artist: "sh-Mashary",
+    img: "images/2.jpg",
+    src: "../records/2.mp3"
+  }
   // {
   //   name: "Record 1",
   //   artist: "First Singer",
   //   img: "images/1.jpg",
-  //   src: "../records/1.mp3"
-  // },
-  // {
-  //   name: "quraan1",
-  //   artist: "Second Singer",
-  //   img: "images/2.jpg",
   //   src: "../records/2.mp3"
   // },
   // {
   //   name: "quraan2",
   //   artist: "Third Singer",
   //   img: "images/3.jpg",
-  //   src: "../records/3.mp3"
+  //   src: "../records/4.mp3"
   // },
-  {
-    name: "Azan",
-    artist: "Fourth Singer",
-    img: "images/4.jpg",
-    src: "../records/4.mp3"
-  }
 ];
