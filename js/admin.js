0c7c097f0e7ade0c9df3af27eec12616773fd833
Ahmed Abdelwanes <ahmedabdalwanes842@gmail.com>
2025-05-05 15:44:48 +0300

    add spinner load

--- js/admin.js
+++ js/admin.js
@@ -11,7 +11,10 @@ document
     try {
       // ملاحظة: الخادم لا يدعم HTTPS، لذا نستخدم وكيل CORS Anywhere كحل مؤقت
       // للإنتاج، يجب تمكين HTTPS على الخادم أو استخدام وكيل مخصص (مثل Cloudflare Workers)
-      document.getElementById("result").innerText = "يتم الرفع الان";
+      document.getElementById(
+        "result"
+      ).innerHTML = ` <i class="fa-solid fa-spinner"></i> <br/>
+      ... يتم الرفع الان`;
       const response = await fetch("https://apidemo.runasp.net/api/Upload", {
         method: "POST",
         body: formData
